9e81f21f1d827213ddb99ed68ce9c654cbc0ce35
Saillphrogg <161702084+Saillphrogg@users.noreply.github.com>
2024-06-06 10:43:45 +0200

    Skibidi rizz
    Little gronk the rizz king 100 100

--- stack.py
+++ stack.py
@@ -1,22 +1,27 @@
 class Stack:
-    """Provides stack container"""
-    def __init__(self):
-        pass
+    def __init__(self,data_type=None):
+            self.stack=[]
+            self.data_type = data_type
 
-    def push(self, data):
-        pass
-
+    def push(self,data):
+        if self.data_type in None or isinstance(data, self.data_type):
+            self.stack.append(data)
+   
     def pop(self):
-        pass
+        if self.stack:
+            self.stack.pop()
+        else:
+            return None
 
     def get(self):
-        pass
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None   
 
     def length(self):
-        pass
-
-    def __str__(self):
-        pass
+        return len(self.stack)
+        
 
     def __repr__(self):
-        pass
+        return repr(self.stack)  
